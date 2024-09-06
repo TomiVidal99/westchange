6b8af7ebc31f95471fd77251429f4fcd718b93a7
Tomás Vidal <tomividal9@gmail.com>
2024-09-06 17:23:31 -0300

    added recomendation about aliases

--- hotkeys.sh
+++ hotkeys.sh
@@ -2,3 +2,7 @@
 # hit: CTRL+W
 function switch_directory_() {zle -I; switch_directory}; zle -N switch_directory_
 bindkey -M vicmd '^w' switch_directory_
+
+# also it's recommended to have some aliases like:
+# alias awcd='add_directory'
+# alias rwcd='remove_directory'
